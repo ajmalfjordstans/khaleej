d695f1841019ab71d52f4b90aafe9c2be714a2a7
ajmalfjordstans <frontend.fjordstans@gmail.com>
2024-01-18 17:06:49 +0530

    preloader upsized

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Loading = () => (
   <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#7b022e] z-[30]">
-    <Image height={300} width={300} src="/Loading.jpg" alt="Logo" className=" w-[70px] md:w-[300px] animate-pulse h-full object-cover"/>
+    <Image height={900} width={1200} src="/Loading.jpg" alt="Logo" className="animate-pulse h-full w-full object-cover"/>
   </div>
 );
 
