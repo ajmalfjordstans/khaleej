bc8558b300ff250b47961da9dbf8c9d193917c4e
ajmalfjordstans <frontend.fjordstans@gmail.com>
2024-01-18 10:38:22 +0530

    instagram gallery

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   images: {
-    domains: ['<backend paths>'],
+    domains: ['<backend paths>','scontent.cdninstagram.com'],
     // unoptimized: true
   },
 }
--- src/app/gallery.jsx
+++ src/app/gallery.jsx
@@ -2,83 +2,59 @@
 
 import applyScrollAnimation from '@/components/scroll-animation'
 import Image from 'next/image'
-import React, { useEffect } from 'react'
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react'
+const axios = require('axios');
 
 export default function Gallery() {
-  // useEffect(() => {
-  //   applyScrollAnimation('.scrollElement4', '.triggerElement4')
-  // }, [])
-  // https://graph.instagram.com/v12.0/khaleejmandi.uk/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink,timestamp&access_token=IGQWROM1VHSG40a3ExY0ZAfckVmZAXQyOXdCaDlWeDlPZAEpLdlB0aDN5N2J3NnpnMl8zTXl3ZAGJ3VTd4TWtxYWVBOUlQTGxRTVlxZAFROYkEteTlhSTRQczhGMUQxNkpUTElRWjhrbVRuVHZAoeUpxdTEzR1lSTmJDQWsZD
+  const [data, setData] = useState()
+  useEffect(() => {
+    applyScrollAnimation('.scrollElement4', '.triggerElement4')
+    const apiEndpoint = 'https://v1.nocodeapi.com/mohammedajmal/instagram/cHdBEvCuLNKJBCxr';
 
-  // https://graph.instagram.com/v12.0/access_token?grant_type=ig_exchange_token&client_id=1558929474869657&client_secret=a54d707033563095679d243919fff172&fb_exchange_token=IGQWROM1VHSG40a3ExY0ZAfckVmZAXQyOXdCaDlWeDlPZAEpLdlB0aDN5N2J3NnpnMl8zTXl3ZAGJ3VTd4TWtxYWVBOUlQTGxRTVlxZAFROYkEteTlhSTRQczhGMUQxNkpUTElRWjhrbVRuVHZAoeUpxdTEzR1lSTmJDQWsZD
-
-
-  // const userId = 'khaleejmandi.uk'
-  // const token = 'IGQWROM1VHSG40a3ExY0ZAfckVmZAXQyOXdCaDlWeDlPZAEpLdlB0aDN5N2J3NnpnMl8zTXl3ZAGJ3VTd4TWtxYWVBOUlQTGxRTVlxZAFROYkEteTlhSTRQczhGMUQxNkpUTElRWjhrbVRuVHZAoeUpxdTEzR1lSTmJDQWsZD'
-  // const apiUrl = `https://graph.instagram.com/v12.0/${userId}/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink,timestamp&access_token=${token}`;
+    axios.get(apiEndpoint)
+      .then(response => {
+        // Handle the response data
+        setData(response.data.data)
+        // console.log(response.data.data);
+      })
+      .catch(error => {
+        // Handle errors
+        console.error('Error:', error.message);
+      });
+  }, [])
   return (
     <section className='container mx-auto px-[5%] lg:px-0 py-20 ' id='gallery'>
       <p className='text-[42px] md:text-[64px] font-[600] leading-normal pb-[40px] text-left font-source scrollElement'>Khaleej Gallery</p>
       <div className='w-full max-w-[310px] h-[3px] bg-[#F3F25B]'></div>
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-4 mt-8 triggerElement4">
-        <Image
-          height={300}
-          width={300}
-          className="h-[100%] max-w-[100%] rounded-lg object-cover"
-          src="/Images/majlis.jpg"
-          alt="gallery-photo"
-        />
-        <Image
-          height={300}
-          width={300}
-          className="h-[100%] max-w-[100%] rounded-lg object-cover"
-          src="/Images/majlis2.jpg"
-          alt="gallery-photo"
-        />
-        <Image
-          height={300}
-          width={300}
-          className="h-[100%] max-w-[100%] rounded-lg object-cover"
-          src="/Images/mandi1.jpg"
-          alt="gallery-photo"
-        />
-        <Image
-          height={300}
-          width={300}
-          className="h-[100%] max-w-[100%] rounded-lg object-cover"
-          src="/Images/landingpage.jpg"
-          alt="gallery-photo"
-        />
-        <Image
-          height={300}
-          width={300}
-          className="h-[100%] max-w-[100%] rounded-lg object-cover"
-          src="/Images/aboutus1.jpg"
-          alt="gallery-photo"
-        />
-        <Image
-          height={300}
-          width={300}
-          className="h-[100%] max-w-[100%] rounded-lg object-cover"
-          src="/Images/carousel/landingpage.jpg"
-          alt="gallery-photo"
-        />
-        <Image
-          height={300}
-          width={300}
-          className="h-[100%] max-w-[100%] rounded-lg object-cover"
-          src="/Images/carousel/carousel3.jpg"
-          alt="gallery-photo"
-        />
-        <Image
-          height={300}
-          width={300}
-          className="h-[100%] max-w-[100%] rounded-lg object-cover"
-          src="/Images/carousel/carousel5.jpg"
-          alt="gallery-photo"
-        />
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-8 triggerElement4">
 
+        {data?.map((post, id) => {
+          return (
+            <Link
+              href={post?.permalink}
+              target="_blank"
+              key={id}
+            >
+              <div className='relative'>
+                <Image
+                  height={300}
+                  width={300}
+                  className="h-[270px] max-w-[100%] rounded-lg object-cover"
+                  src={post?.media_type == 'IMAGE' ? post?.media_url : post?.thumbnail_url}
+                  alt="gallery-photo"
+                />
+                {post?.media_type === 'VIDEO' && (
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="white" className='absolute top-5 right-8 h-[26px] w-[26px]'>
+                    <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
+                  </svg>
+                )}
+
+              </div>
+            </Link>
+          )
+        })}
       </div>
-    </section>
+    </section >
   )
 }
