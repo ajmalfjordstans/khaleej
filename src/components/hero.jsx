f28ceba0bcce4d4da99b37c435669f25fdd802ce
ajmalfjordstans <frontend.fjordstans@gmail.com>
2024-01-22 18:29:47 +0530

    landing carousel

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,7 @@
 
 import { Button } from '@material-tailwind/react'
 import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 import React, { useEffect, useState } from 'react'
 import GoogleMapComponent from './google-map'
@@ -11,14 +11,16 @@ import Loading from './loading'
 export default function Hero() {
   const [loading, setLoading] = React.useState(false)
   const [bg, setBg] = useState('/Images/carousel/landingpage.jpg')
+  const [carouselBg, setCarouselBg] = useState('/Images/carousel/landingpage.jpg')
   const pathname = usePathname()
-  // const router = useRouter()
-  // console.log(router.events);
   let carousel = ['/Images/carousel/landingpage.jpg', '/Images/carousel/carousel3.jpg', '/Images/carousel/carousel5.jpg']
 
   useEffect(() => {
     // setBg('/Images/carousel/landingpage.jpg') startSlide()
-    if (pathname == '/') setBg('/Images/carousel/landingpage.jpg')
+    if (pathname == '/') {
+      setBg('/Images/carousel/landingpage.jpg')
+      startSlide()
+    }
     else if (pathname == '/about') setBg('/Images/newimages/aboutus1.jpg')
     else if (pathname == '/menu') setBg('/Images/newimages/ourmenu.jpg')
     else if (pathname == '/contact') setBg('/Images/newimages/contactus.jpg')
@@ -30,7 +32,7 @@ export default function Hero() {
 
     const intervalId = setInterval(() => {
       currentIndex = (currentIndex + 1) % carousel.length;
-      setBg(carousel[currentIndex]);
+      setCarouselBg(carousel[currentIndex]);
     }, 5000);
     if (pathname !== '/') clearInterval(intervalId)
     return () => clearInterval(intervalId);
@@ -49,22 +51,23 @@ export default function Hero() {
   }
 
   const content = () => {
-    if (pathname == '/') {
-      return (
-        <div className='text-center'>
-          <p className={`font-julius text-[42px] md:text-[64px] font-[300] leading-normal uppercase`}>Khaleej<br /> Mandi House</p>
-          <p className='font-julius text-[15px] md:text-[24px] font-[600] uppercase mt-[20px]'>The Authentic Mandi in United Kingdom </p>
-          <p className='font-julius text-[15px] md:text-[24px] font-[600] uppercase '>Flavours of royalty</p>
-          <Link
-            href="/menu"
-          >
-            <Button
-              className='bg-primary rounded-[5px] w-[333px] mt-[40px] capitalize font-[400] font-source'
-            >View Menu</Button>
-          </Link>
-        </div>
-      )
-    } else if (pathname == '/about') {
+    // if (pathname == '/') {
+    //   return (
+    //     <div className='text-center'>
+    //       <p className={`font-julius text-[42px] md:text-[64px] font-[300] leading-normal uppercase`}>Khaleej<br /> Mandi House</p>
+    //       <p className='font-julius text-[15px] md:text-[24px] font-[600] uppercase mt-[20px]'>The Authentic Mandi in United Kingdom </p>
+    //       <p className='font-julius text-[15px] md:text-[24px] font-[600] uppercase '>Flavours of royalty</p>
+    //       <Link
+    //         href="/menu"
+    //       >
+    //         <Button
+    //           className='bg-primary rounded-[5px] w-[333px] mt-[40px] capitalize font-[400] font-source'
+    //         >View Menu</Button>
+    //       </Link>
+    //     </div>
+    //   )
+    // } else
+    if (pathname == '/about') {
       return (template(
         'ABOUT US',
         '"Welcome to our world of Mandi, where we craft diverse flavours,tradition and togetherness of the Khaleej in perfect harmony."'
@@ -91,23 +94,45 @@ export default function Hero() {
     }
   }
 
-  return (
-    <section className='h-[100dvh] w-[100vw] text-white  transition-all duration-500 px-[5%]' id='hero'
-      style={{ backgroundImage: `url("${bg}")`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-    >
-      <div className='absolute inset-0 bg-black opacity-20 z-[9]'></div>
-      <div className='relative z-[10] h-[100dvh] flex flex-col justify-center'>
-        {content()}
-        <div className='w-full flex flex-col items-center'>
+
+  if (pathname == '/')
+    return (
+      <section className='h-[100dvh] w-[100vw] text-white  transition-all duration-500 px-[5%] flex items-center justify-center' id='hero'
+        style={{ backgroundImage: `url("${carouselBg}")`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+      >
+        <div className='absolute inset-0 bg-black opacity-20 z-[9]'></div>
+        <div className='text-center z-[10]'>
+          <p className={`font-julius text-[42px] md:text-[64px] font-[300] leading-normal uppercase`}>Khaleej<br /> Mandi House</p>
+          <p className='font-julius text-[15px] md:text-[24px] font-[600] uppercase mt-[20px]'>The Authentic Mandi in United Kingdom </p>
+          <p className='font-julius text-[15px] md:text-[24px] font-[600] uppercase '>Flavours of royalty</p>
           <Link
-            href='#nav'
+            href="/menu"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 absolute bottom-[40px] mx-auto animate-bounce hover:cursor-pointer">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-            </svg>
+            <Button
+              className='bg-primary rounded-[5px] w-[333px] mt-[40px] capitalize font-[400] font-source'
+            >View Menu</Button>
           </Link>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  else
+    return (
+      <section className='h-[100dvh] w-[100vw] text-white  transition-all duration-500 px-[5%]' id='hero'
+        style={{ backgroundImage: `url("${bg}")`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+      >
+        <div className='absolute inset-0 bg-black opacity-20 z-[9]'></div>
+        <div className='relative z-[10] h-[100dvh] flex flex-col justify-center'>
+          {content()}
+          <div className='w-full flex flex-col items-center'>
+            <Link
+              href='#nav'
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 absolute bottom-[40px] mx-auto animate-bounce hover:cursor-pointer">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </section>
+    )
 }
