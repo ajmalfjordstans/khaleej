6bb8583f70383bea007f7a3c51d3311d6f958a26
ajmalfjordstans <frontend.fjordstans@gmail.com>
2026-04-21 14:20:30 +0530

    drawer hidden issue

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,8 +4,6 @@ import Link from 'next/link'
 import React from 'react'
 import {
   Drawer,
-  Button,
-  Typography,
   IconButton,
 } from "@material-tailwind/react";
 import { Bars3Icon } from "@heroicons/react/24/outline"
@@ -15,70 +13,46 @@ export default function Navbar() {
 
   const openDrawer = () => setOpen(true);
   const closeDrawer = () => setOpen(false);
-  const clickHandle = () => {
-    return (
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-      })
-    )
-  }
+
   return (
-    // <div className='relative'>
-    <div className='container-px flex items-center justify-evenly text-[14px] font-[600] text-primary sticky top-0 md:top-2 glass-nav z-[20] h-[60px] md:h-[80px] transition-all duration-300 md:rounded-full md:mx-auto md:max-w-[95%] shadow-premium mt-0 md:mt-2'>
-      <div className='hidden lg:block w-[40%]'>
-        <div className='flex w-full justify-around'>
-          <Link
-            // onClick={() => clickHandle()}
-            href="/"
-          >Home</Link>
-          <Link
-            // onClick={() => clickHandle()}
-            href="/about"
-          >About</Link>
-          <Link
-            // onClick={() => clickHandle()}
-            href="/menu"
-          >Menu</Link>
+    <>
+      <div className='container-px flex items-center justify-evenly text-[14px] font-[600] text-primary sticky top-0 md:top-2 glass-nav z-[20] h-[60px] md:h-[80px] transition-all duration-300 md:rounded-full md:mx-auto md:max-w-[95%] shadow-premium mt-0 md:mt-2'>
+        <div className='hidden lg:block w-[40%]'>
+          <div className='flex w-full justify-around'>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/menu">Menu</Link>
+          </div>
+        </div>
+        <Link href="/">
+          <Image
+            src='/Logo.png'
+            height={101}
+            width={162}
+            alt='logo'
+            className='w-[160px]'
+          />
+        </Link>
+        <div className='hidden lg:block w-[40%]'>
+          <div className='flex w-full justify-around'>
+            <Link href="/contact">Contact</Link>
+            <Link href="/careers">Careers</Link>
+            <Link href="tel:+44 116 507 4571">Call Us</Link>
+          </div>
+        </div>
+        <div className='absolute right-4 lg:hidden'>
+          <Bars3Icon onClick={openDrawer} className='h-[30px] w-[30px]' />
         </div>
       </div>
-      <Link
-        href="/"
-      // onClick={() => clickHandle()}
-      >
-        <Image
-          src='/Logo.png'
-          height={101}
-          width={162}
-          alt='logo'
-          className='w-[160px]'
-        />
-      </Link>
-      <div className='hidden lg:block w-[40%]'>
-        <div className='flex w-full justify-around'>
-          <Link
-            // onClick={() => clickHandle()}
-            href="/contact"
-          >Contact</Link>
-          <Link
-            // onClick={() => clickHandle()}
-            href="/careers"
-          >Careers</Link>
-          {/* <Link
-              // onClick={() => clickHandle()}
-              href="/book-now"
-            >Book Now</Link> */}
-          <Link
-            href="tel:+44 116 507 4571"
-          >Call Us</Link>
-        </div>
-      </div>
-      <div className='absolute right-4 lg:hidden'>
-        <Bars3Icon onClick={openDrawer} className='h-[30px] w-[30px]' />
-      </div>
+
       <div className='lg:hidden'>
-        <Drawer open={open} onClose={closeDrawer} placement="right" className="p-4">
+        <Drawer
+          open={open}
+          onClose={closeDrawer}
+          placement="right"
+          className="p-4 bg-white"
+          style={{ zIndex: 9999 }}
+        >
           <div className="mb-6 flex flex-col items-center justify-between">
             <div className='flex items-center justify-between w-full'>
               <Image
@@ -105,50 +79,17 @@ export default function Navbar() {
                 </svg>
               </IconButton>
             </div>
-            <div className='flex flex-col w-full gap-[20px] px-[5%]'>
-              <Link
-                onClick={() => {
-                  closeDrawer()
-                  // clickHandle()
-                }}
-                href="/"
-              >Home</Link>
-              <Link
-                onClick={() => {
-                  closeDrawer()
-                  // clickHandle()
-                }}
-                href="/about"
-              >About</Link>
-              <Link
-                onClick={() => {
-                  closeDrawer()
-                  // clickHandle()
-                }}
-                href="/menu"
-              >Menu</Link>
-              <Link
-                onClick={() => {
-                  closeDrawer()
-                  // clickHandle()
-                }}
-                href="/contact"
-              >Contact</Link>
-              <Link
-                onClick={() => {
-                  closeDrawer()
-                  // clickHandle()
-                }}
-                href="/careers"
-              >Careers</Link>
-              <Link
-                href="tel:+44 116 507 4571"
-              >Call Us</Link>
+            <div className='flex flex-col w-full gap-[20px] px-[5%] mt-4'>
+              <Link onClick={closeDrawer} href="/">Home</Link>
+              <Link onClick={closeDrawer} href="/about">About</Link>
+              <Link onClick={closeDrawer} href="/menu">Menu</Link>
+              <Link onClick={closeDrawer} href="/contact">Contact</Link>
+              <Link onClick={closeDrawer} href="/careers">Careers</Link>
+              <Link href="tel:+44 116 507 4571">Call Us</Link>
             </div>
           </div>
         </Drawer>
       </div>
-    </div>
-    // </div>
+    </>
   )
 }
